59062fca653477327d496fbcdeccee6bc77c48d2
John Pavley <john@pavley.com>
2014-09-29 13:54:05 -0400

    Initial check in

---
+++ app.js
@@ -0,0 +1,9 @@
+// app.js
+// No Fear JavaScript
+// @jpavley
+
+var realBird = { name: "Blue Jay", song: "Tweet" };
+var fakeBird = { name: "Mocking Jay", song: "La" };
+
+console.log(realBird);
+console.log(fakeBird);
